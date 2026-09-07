d459a48647072353e52536b0729c2d45efb47894
Hoang Phuoc Vu - Danny <107795987+phuocvu911@users.noreply.github.com>
2026-09-08 00:54:07 +0300

    ref: switch to repo + handler layers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,15 @@
 package handlers
+
+import (
+	"html/template"
+	"lions/internal/repository"
+)
+
+type Handler struct {
+	repo      *repository.Repository
+	templates map[string]*template.Template
+}
+
+func NewHandler(repo *repository.Repository, templates map[string]*template.Template) *Handler {
+	return &Handler{repo: repo, templates: templates}
+}
