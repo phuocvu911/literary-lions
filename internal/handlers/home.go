2e0a803f306e0f121721d56d3eced91dd407a2aa
Hoang Phuoc Vu - Danny <107795987+phuocvu911@users.noreply.github.com>
2026-09-09 16:56:45 +0300

    feat: home page

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -1,1 +1,15 @@
-pa
+package handlers
+
+import (
+	"net/http"
+)
+
+func (app *App) HandleHome(w http.ResponseWriter, r *http.Request) {
+	user := app.currentUser(r)
+
+	//do something
+
+	app.render(w, "home.html", map[string]any{
+		"User": user,
+	})
+}
